4247cdd214e592ab169d1933133301a074028204
Jaya Wijono from Dot Kingdom <jaya@hhd.com.au>
2015-10-15 02:08:14 +1100

    add reset button function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var $timer = $('#timer'),
     time = 2 * 60,
-    formattedTime = formatTime(time*1000);
+    formattedTime = formatTime(time*1000),
+    formattedTickedTime = [];
 
 function formatTime(millisec) {
   var min = parseInt(millisec / 60000,10),
@@ -11,25 +12,39 @@ function formatTime(millisec) {
   return [min, sec];
 }
 
+function displayTimer(time) {
+  $timer.html('<span>'+time[0]+':'+time[1]+'</span>');
+}
+
 var timer = new Timer({
   tick: 1,
   ontick: function(millisec) {
-    formattedTime = formatTime(millisec);
-    $timer.html('<span>'+formattedTime[0]+':'+formattedTime[1]+'</span>');
+    formattedTickedTime = formatTime(millisec);
+    $timer.html('<span>'+formattedTickedTime[0]+':'+formattedTickedTime[1]+'</span>');
   },
   onstart: function() {
     Game.canPick = true;
     console.log('timer started');
   },
-  onend: function() {
+  onstop: function(){
     console.log('timer stopped');
+  },
+  onend: function() {
+    console.log('timer finished');
     Game.canPick = false;
     $timer.html('<span>Time finished!</span>');
   }
 });
 
-$timer.html('<span>'+formattedTime[0]+':'+formattedTime[1]+'</span>');
+displayTimer(formattedTime);
 
 $('#start-button').on('click', function(){
+  timer.start(time);
+});
+
+$('#reset-button').on('click', function(){
+  timer.stop();
+  displayTimer(formattedTime);
+  Game.init(cards);
   timer.start(time);
 });
